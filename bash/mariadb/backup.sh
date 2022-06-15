e99a606aa3255bf81f7e53867f8e472ac46ce67c
palminalbert333 <palmin.albert@yandex.ru>
2022-06-15 12:14:09 +0300

    .

--- bash/mariadb/backup.sh
+++ bash/mariadb/backup.sh
@@ -3,4 +3,4 @@ mariabackup --backup \
    --target-dir=/home/albert/backup/db_backup_$(date +%Y-%m-%d-%H-%M)/ \
    --user=root --password=15426378
 
-#mysql -u root UPDATE mysql.user SET Grant_priv = 'Y', Super_priv = 'Y' WHERE User = 'zabbix';
+#UPDATE mysql.user SET Grant_priv = 'Y', Super_priv = 'Y' WHERE User = 'zabbix';
