8afd4d505560a41b5f381bca84ace223413ef563
palminalbert333 <palmin.albert@yandex.ru>
2022-06-16 15:38:16 +0300

    .

--- bash/smart-raid.sh
+++ bash/smart-raid.sh
@@ -4,12 +4,12 @@ if /opt/adaptec/arcconf GETSMARTSTATS 1 TABULAR | grep Passed;
 then
   echo "SMART is GOOD!"
 else
-  curl -s "https://api.telegram.org/bot2023332846:AAGwBcjFHxeRsMdKhZ1FqVDeROBxU17w4KA/sendMessage?chat_id=-1001612626954&text=Proxmox2%20SMART%20is%20BAD!"
+  curl -s "https://api.telegram.org/bot<my_token>/sendMessage?chat_id=-1001612626954&text=Proxmox2%20SMART%20is%20BAD!"
 fi
 
 if /opt/adaptec/arcconf GETCONFIG 1 AL | grep Temperature | grep Normal;
 then
   echo "Temperature RAID controller is GOOD!"
 else
-  curl -s "https://api.telegram.org/bot2023332846:AAGwBcjFHxeRsMdKhZ1FqVDeROBxU17w4KA/sendMessage?chat_id=-1001612626954&text=Proxmox2%20Temperature%20RAID%20controller%20is%20BAD!"
+  curl -s "https://api.telegram.org/bot<my_token>/sendMessage?chat_id=-1001612626954&text=Proxmox2%20Temperature%20RAID%20controller%20is%20BAD!"
 fi
