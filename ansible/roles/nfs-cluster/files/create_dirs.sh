70e8968343c245d03a3cea118942369e9be1ebcc
palminalbert333 <palmin.albert@yandex.ru>
2022-06-24 14:58:53 +0300

    .

--- ansible/roles/nfs-cluster/files/create_dirs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir /var/nfs
-mkdir /var/nfs/kubernetes
-
--- ansible/roles/nfs-cluster/files/drbd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-systemctl enable drbd
-#systemctl start drbd
-drbdadm create-md nfsdrbd && modprobe drbd && drbdadm up nfsdrbd > /tmp/drbd.log
